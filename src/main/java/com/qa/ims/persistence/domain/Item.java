9b9d1db23a6b40aaee29b0c4e0656ebd79f47ba8
Callum <callumjrobinson2020@gmail.com>
2022-03-29 13:06:43 +0100

    IP-16 #comment added the other item constructors and the getters and setters

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -8,12 +8,31 @@ public class Item {
 	private String description;
 	private Float cost;
 	
+	// Constructor with needed fields
 	public Item(String type, String name, Float cost) {
 		this.type = type;
 		this.name = name;
 		this.cost = cost;
 	}
 
+	/*
+	 * Other constructors
+	 */
+	public Item(Long id, String type, String name, Float cost) {
+		this.id = id;
+		this.type = type;
+		this.name = name;
+		this.cost = cost;
+	}
+	
+	public Item(String type, String name, String description, Float cost) {
+		super();
+		this.type = type;
+		this.name = name;
+		this.description = description;
+		this.cost = cost;
+	}
+	
 	public Item(Long id, String type, String name, String description, Float cost) {
 		this.id = id;
 		this.type = type;
@@ -21,6 +40,52 @@ public class Item {
 		this.description = description;
 		this.cost = cost;
 	}
+	
+	
+	/*
+	 * getters and setters
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Float getCost() {
+		return cost;
+	}
+
+	public void setCost(Float cost) {
+		this.cost = cost;
+	}
+	
+	
 	
 	
 }
