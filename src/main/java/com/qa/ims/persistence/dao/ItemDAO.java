a6b59d5c60c7639d51e598adb0629756573beb2d
Callum <callumjrobinson2020@gmail.com>
2022-03-29 14:33:53 +0100

    IP-16 #comment created ItemDao class and generated the inherited methods from the Dao interface

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,53 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+
+public class ItemDAO implements Dao<Item> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item create(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
