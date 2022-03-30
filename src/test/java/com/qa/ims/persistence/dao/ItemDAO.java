3cd0d145833d2ce4afe3d936f0c2c6186df92f2e
Callum <callumjrobinson2020@gmail.com>
2022-03-30 10:01:26 +0100

    IP-28 #comment updated test SQL schema

---
+++ src/test/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,8 @@
+package com.qa.ims.persistence.dao;
+
+public class ItemDAO {
+
+	private final ItemDAO DAO = new ItemDAO();
+	
+	
+}
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,37 @@
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `orderitems`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
+    `first_name` VARCHAR(64) NOT NULL,
+    `surname` VARCHAR(64) NOT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(40) NOT NULL,
+    `name` VARCHAR(40) NOT NULL,
+    `description` VARCHAR(256),
+    `cost` DECIMAL(4,2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    `date_placed` DATE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orderitems` (
+    `order_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    `quantity` INT DEFAULT 1,
+    PRIMARY KEY (`order_id`, `item_id`),
+    FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
 );
