8fd7449052ce4c39b6c149b216626b07b2bc28c2
Callum <callumjrobinson2020@gmail.com>
2022-03-31 15:30:47 +0100

    IP-20 #comment added a order choice enum for use in the order controller

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,91 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.persistence.domain.OrderChoice;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	/*
+	 * The Order controller requires the Order DAO and utils
+	 */
+	private OrderDAO orderDAO;
+	private Utils utils;
+	
+	/*
+	 * Generated constructor from two fields
+	 */
+	public OrderController(OrderDAO orderDAO, Utils utils) {
+		super();
+		this.orderDAO = orderDAO;
+		this.utils = utils;
+	}
+	
+	
+	/*
+	 * Creates an order instance with user entered fields
+	 * Gives option to add a whole order or just an order item
+	 * 
+	 * @return the order
+	 */
+	@Override
+	public Order create() {
+		
+		OrderChoice choice = null;
+		do {
+			LOGGER.info("Which would you like to create?");
+			OrderChoice.printOrderChoices();
+			
+			choice = OrderChoice.getOrderChoice(utils);
+			
+			switch (choice) {
+			case ORDER:
+				LOGGER.info("Please enter the customer id");
+				String customerId = utils.getString();
+				
+				return null;
+			case ITEM:
+				LOGGER.info("Do That");
+				return null;
+			case STOP:
+				return null;
+			default:
+				LOGGER.info("ERROR");
+				break;
+			}
+			
+		} while (choice != OrderChoice.STOP);
+		
+		return null;
+	}
+	
+	
+	
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/com/qa/ims/persistence/domain/OrderChoice.java
@@ -0,0 +1,62 @@
+package com.qa.ims.persistence.domain;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.utils.Utils;
+
+public enum OrderChoice {
+
+	/*
+	 * Choose between orders, items in an order or to return
+	 */
+	ORDER("A whole order"), ITEM("Items in an order"), STOP("To return");
+	
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public String description;
+	
+	/*
+	 * Constructor for the enum
+	 */
+	private OrderChoice(String description) {
+		this.description = description;
+	}
+	
+	/*
+	 * get description for printing
+	 */
+	public String getDescription() {
+		return this.name() + ": " + this.description;
+	}
+	
+	/*
+	 * print choices
+	 */
+	public static void printOrderChoices() {
+		for (OrderChoice choice : OrderChoice.values()) {
+			LOGGER.info(choice.getDescription());
+		}
+	}
+	
+	
+	/*
+	 * pick the choice
+	 * 
+	 * @param utils - uses the utils class to read user input
+	 * 
+	 * @return the choice
+	 */
+	public static OrderChoice getOrderChoice(Utils utils) {
+		OrderChoice choice;
+		while (true) {
+			try {
+				choice = OrderChoice.valueOf(utils.getString().toUpperCase());
+				break;
+			} catch (IllegalArgumentException e) {
+				LOGGER.error("Invalid selection please try again");
+			}
+		}
+		return choice;
+	}
+}
