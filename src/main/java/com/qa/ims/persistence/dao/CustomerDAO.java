1b2a0a8da19070c1e399e59fbe6d5694292d59f1
Callum <callumjrobinson2020@gmail.com>
2022-04-01 19:32:02 +0100

    IP-33 #comment added functionality that removes orders when a customer is removed

--- src/main/java/com/qa/ims/persistence/dao/CustomerDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/CustomerDAO.java
@@ -128,6 +128,8 @@ public class CustomerDAO implements Dao<Customer> {
 	 */
 	@Override
 	public int delete(long id) {
+		OrderDAO orderDAO = new OrderDAO();
+		orderDAO.correctOrder(id);
 		try (Connection connection = DBUtils.getInstance().getConnection();
 				PreparedStatement statement = connection.prepareStatement("DELETE FROM customers WHERE id = ?");) {
 			statement.setLong(1, id);
