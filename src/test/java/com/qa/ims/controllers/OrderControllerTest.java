cc1c91370444ee074e8515333425929b0643b340
Callum <callumjrobinson2020@gmail.com>
2022-04-05 17:35:09 +0100

    IP-28 added test for delete in order controller

--- src/test/java/com/qa/ims/controllers/OrderControllerTest.java
+++ src/test/java/com/qa/ims/controllers/OrderControllerTest.java
@@ -1,8 +1,12 @@
 package com.qa.ims.controllers;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.controller.OrderController;
@@ -23,4 +27,21 @@ public class OrderControllerTest {
 	
 	@InjectMocks
 	private OrderController controller;
+	
+
+	/*
+	 * Test the delete method in the Order controller by mocking the user input and DAO delete method
+	 */
+	@Test
+	public void testDelete() {
+		final long ID = 1L;
+		
+		Mockito.when(utils.getLong()).thenReturn(ID);
+		Mockito.when(dao.delete(ID)).thenReturn(1);
+		
+		assertEquals(1, this.controller.delete());
+		
+		Mockito.verify(utils, Mockito.times(1)).getLong();
+		Mockito.verify(dao, Mockito.times(1)).delete(ID);
+	}
 }
