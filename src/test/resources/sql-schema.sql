112d7387e3650d533ea63f1e24b29782377d5aac
Callum <callumjrobinson2020@gmail.com>
2022-04-05 10:15:43 +0100

    IP-28 #comment updated SQL schema for test database

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS `customers`;
 DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `orderitems`;
+
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -15,4 +18,22 @@ CREATE TABLE IF NOT EXISTS `items` (
     `description` VARCHAR(256),
     `cost` DECIMAL(4,2) NOT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `customer_id` BIGINT NOT NULL,
+    `date_placed` DATE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `orderitems` (
+    `order_id` BIGINT NOT NULL,
+    `item_id` BIGINT NOT NULL,
+    `quantity` INT DEFAULT 1,
+    PRIMARY KEY (`order_id`, `item_id`),
+    FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
 );
